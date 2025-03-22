8e98af694d0f58af14edbf740d3754f45fe27403
kliba <klibaner@mail.com>
2025-03-22 14:20:21 +0200

    hello

--- src/data/hero.ts
+++ src/data/hero.ts
@@ -1,5 +1,5 @@
 export const heroDetails = {
-    heading: 'Smart, Secure, Simple Financial Management',
+    heading: 'Hello, Secure, Simple Financial Management',
     subheading: 'From effortless budgeting to real-time investment insights, Finwise puts you in control of your money like never before',
     centerImageSrc: '/images/hero-mockup.webp',
 }
